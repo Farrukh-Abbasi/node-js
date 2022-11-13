6f86549ef29310ea3acf878e4a267c63404bf96c
hinanaz5 <hina.hrm87@gmail.com>
2022-11-13 17:55:20 +0500

    server

---
+++ server.mjs
@@ -0,0 +1,14 @@
+import  express  from 'express';
+const app = express()
+const port = 3000
+
+app.get('/water', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/food', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
